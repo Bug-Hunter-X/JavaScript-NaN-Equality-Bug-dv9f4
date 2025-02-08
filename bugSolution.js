49e41a40556832cd40a0142caf7564ba616baa94
BugHunter-X <askzenai@gmail.com>
2025-02-08 10:48:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (Number.isNaN(a) && Number.isNaN(b)) {
+    return true;
+  } else if (a === b) {
+    return true;
+  }
+  return false;
+}
+
+console.log(foo(NaN, NaN)); // Output: true
+console.log(foo(1,1)); //Output: true
+console.log(foo(1,2)); //Output: false
